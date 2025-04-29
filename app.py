934f13058d81dc7f8b3ff5118a7e2ec9c80acc4a
Thiago3011 <thiago.silva1001@outlook.com>
2025-04-29 19:56:17 -0300

    adding and configuring websockets

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-from flask_socketio import SocketIO
+from flask_socketio import SocketIO, send
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'admin123'
@@ -7,7 +7,12 @@ socketio = SocketIO(app)
 
 @app.route('/')
 def index():
-    return "Servidor iniciado"
+    return render_template('index.html')
+
+@socketio.on('message')
+def handle_message(message):
+    print(f"Mensagem {message} recebida")
+    send(f"Thiago: {message}")
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
